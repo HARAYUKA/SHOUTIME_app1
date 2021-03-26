357b953714eb55e9fbb3e60f677d93ba0f6c7b38
kwansai724 <kwansai.taka724@gmail.com>
2021-03-26 22:50:13 +0900

    Diagnosesコントローラー(index)、gemファイル追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,33 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
+$(function(){
+  $radio = $(':radio');
+  //$radio.click(check);//チェックと連動させる
+  $('#check').click(check);//診断ボタン押させる
+  
+  function check(){
+      var total = 0;
+      var result = '';
+      
+      $radio.filter(':checked').map(function(){
+          total += Number(this.value);
+      });
+      
+      if (total > 6) {
+          result = 'ｷﾀ━(ﾟ∀ﾟ)━!';
+      } else if( total > 3 ) {
+          result = '(・∀・)ｲｲ!';
+      } else if( total > 0) {
+          result = '( ´_ゝ｀)';
+      } else {
+          result = '何も選択されていません';
+      }
+          
+      $('#result').html(result);
+  }
+});
