1dab2e73157d8b0eb85fbc994debfd4487aa096f
kwansai724 <kwansai.taka724@gmail.com>
2021-03-26 23:49:46 +0900

    かんたん診断①コメントアウト

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,29 +19,29 @@
 //= require bootstrap-sprockets
 
 //かんたん診断
-$(function(){
-  $radio = $(':radio');
+//$(function(){
+//  $radio = $(':radio');
   //$radio.click(check);//チェックと連動させる
-  $('#check').click(check);//診断ボタン押させる
+//  $('#check').click(check);//診断ボタン押させる
   
-  function check(){
-      var total = 0;
-      var result = '';
+//  function check(){
+//      var total = 0;
+//      var result = '';
       
-      $radio.filter(':checked').map(function(){
-          total += Number(this.value);
-      });
+//      $radio.filter(':checked').map(function(){
+//          total += Number(this.value);
+//      });
       
-      if (total > 6) {
-          result = '〇あなたはもしかしたらこんな「目」かも';
-      } else if( total > 3 ) {
-          result = '〇あなたはもしかしたらこんな「目」かも';
-      } else if( total > 0) {
-          result = '〇あなたはもしかしたらこんな「目」かも';
-      } else {
-          result = '何も選択されていません';
-      }
+//      if (total > 6) {
+//          result = '〇あなたはもしかしたらこんな「目」かも';
+//      } else if( total > 3 ) {
+//          result = '〇あなたはもしかしたらこんな「目」かも';
+//      } else if( total > 0) {
+//          result = '〇あなたはもしかしたらこんな「目」かも';
+//      } else {
+//          result = '何も選択されていません';
+//      }
           
-      $('#result').html(result);
-  }
-});
+//      $('#result').html(result);
+//  }
+//});
