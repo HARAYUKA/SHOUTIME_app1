00f9438fe004f49563ba9cb9166cdfcc223c3d52
kwansai724 <kwansai.taka724@gmail.com>
2021-04-03 14:20:13 +0900

    [add]diagnosisのnew/edit/destroyアクション追加

--- app/controllers/diagnoses_controller.rb
+++ app/controllers/diagnoses_controller.rb
@@ -1,27 +1,55 @@
 class DiagnosesController < ApplicationController
   protect_from_forgery except: :index
+  before_action :set_diagnosis, only: [:edit, :update, :destroy]
   
   def index
-    @diagnoses = Diagnosis.all
+    @diagnoses = Diagnosis.all.order(:question_number)
   end
 
   def show
-    @diagnoses = Diagnosis.all
+    @diagnoses = Diagnosis.all.order(:question_number)
   end
 
   def new
+    @diagnosis = Diagnosis.new
   end
 
   def create
+    @diagnosis = Diagnosis.new(diagnosis_params)
+    if @diagnosis.save
+      flash[:success] = "質問を新規作成しました。"
+      redirect_to @diagnosis
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @diagnosis.update_attributes(diagnosis_params)
+      flash[:success] = "質問内容を修正しました。"
+      redirect_to @diagnosis
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @diagnosis.destroy
+    flash[:danger] = "#{@diagnosis.question_number}の質問を削除しました"
+    redirect_to diagnosis_url(@diagnosis)
   end
+
+  private
+
+    def set_diagnosis
+      @diagnosis = Diagnosis.find(params[:id])
+    end
+
+    def diagnosis_params
+      params.require(:diagnosis).permit(:question_number, :question_content)
+    end
   
 end
--- app/models/diagnosis.rb
+++ app/models/diagnosis.rb
@@ -1,2 +1,5 @@
 class Diagnosis < ApplicationRecord
+
+  validates :question_number, presence: true, uniqueness: true
+  validates :question_content, presence: true, uniqueness: true
 end
