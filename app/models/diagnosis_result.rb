5e0445e12ebed9bac901291fcd1be4b6dbc646b1
kwansai724 <kwansai.taka724@gmail.com>
2021-04-06 22:31:52 +0900

    [add]diagnosis_resultモデルRspec追加

--- app/models/diagnosis_result.rb
+++ app/models/diagnosis_result.rb
@@ -1,3 +1,7 @@
 class DiagnosisResult < ApplicationRecord
   mount_uploader :result_picture, ResultPictureUploader
+
+  validates :result_text, presence: true
+  validates :result_picture, presence: true
+  validates :result_type, presence: true
 end
