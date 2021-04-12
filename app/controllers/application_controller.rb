a2eed96a84731e173ab8d19649946a1fcc03b7c7
kwansai724 <kwansai.taka724@gmail.com>
2021-04-12 21:28:52 +0900

    [add]マイページ画面(show.html.erb）作成。ログイン、ログアウト後の画面設定。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  # ログイン後の画面遷移
+  def after_sign_in_path_for(resource)
+    user_url(resource)
+  end
+
+  # ログイン後の画面遷移
+  def after_sign_out_path_for(resource)
+    root_url
+  end
 end
