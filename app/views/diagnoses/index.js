def0db02d967af2466ed3bf097aae1bc178fb395
kwansai724 <kwansai.taka724@gmail.com>
2021-04-18 11:45:51 +0900

    診断結果の表示仕様変更

--- app/views/diagnoses/index.js
+++ app/views/diagnoses/index.js
@@ -1,10 +1,10 @@
 $(function(){
   //一度結果を非表示にする
-  $(".result").hide();
+  $(".result, .results").hide();
   //ボタンがクリックされた時
   $("button").on("click", function(){
     //一度結果を非表示にする
-    $(".result").hide();
+    $(".result, .results").hide();
     //問題数を取得
     var qNum = $("ul.questions li").length;
     if( $("ul.questions li input:checked").length < qNum ){
@@ -20,22 +20,27 @@ $(function(){
             typeENum = $(".左下:checked").length,
             typeFNum = $(".右下:checked").length,
             typeGNum = $(".輻輳開散:checked").length;
+        var a = [typeANum, typeBNum, typeCNum, typeDNum, typeENum, typeFNum, typeGNum];
+        var max = Math.max.apply(null, a);
+        console.log(max);
         if( typeANum > typeBNum && typeANum > typeCNum && typeANum > typeDNum && typeANum > typeENum && typeANum > typeFNum && typeANum > typeGNum) {
-            $(".左上（結果）").fadeIn();
+            $(".左上（結果）,.results").fadeIn();
         } else if( typeBNum > typeANum && typeBNum > typeCNum && typeBNum > typeDNum && typeBNum > typeENum && typeBNum > typeFNum && typeBNum > typeGNum) {
-            $(".右上（結果）").fadeIn();
+            $(".右上（結果）,.results").fadeIn();
         } else if( typeCNum > typeANum && typeCNum > typeBNum && typeCNum > typeDNum && typeCNum > typeENum && typeCNum > typeFNum && typeCNum > typeGNum) {
-            $(".左側（結果）").fadeIn();
+            $(".左側（結果）,.results").fadeIn();
         } else if( typeDNum > typeANum && typeDNum > typeBNum && typeDNum > typeCNum && typeDNum > typeENum && typeDNum > typeFNum && typeDNum > typeGNum) {
-            $(".右側（結果）").fadeIn();
+            $(".右側（結果）,.results").fadeIn();
         } else if( typeENum > typeANum && typeENum > typeBNum && typeENum > typeCNum && typeENum > typeDNum && typeENum > typeFNum && typeENum > typeGNum) {
-            $(".左下（結果）").fadeIn();
+            $(".左下（結果）,.results").fadeIn();
         } else if( typeFNum > typeANum && typeFNum > typeBNum && typeFNum > typeCNum && typeFNum > typeDNum && typeFNum > typeENum && typeFNum > typeGNum) {
-            $(".右下（結果）").fadeIn();
+            $(".右下（結果）,.results").fadeIn();
         } else if( typeGNum > typeANum && typeGNum > typeBNum && typeGNum > typeCNum && typeGNum > typeDNum && typeGNum > typeENum && typeGNum > typeFNum) {
-            $(".輻輳開散（結果）").fadeIn();
-        } else if( typeANum = typeBNum = typeCNum = typeDNum = typeENum = typeFNum = typeGNum) {
-            $(".左上（結果）,.右上（結果）,.左側（結果）,.右側（結果）,.左下（結果）,.右下（結果）,.輻輳開散（結果）").fadeIn();
+            $(".輻輳開散（結果）,.results").fadeIn();
+        } else if( typeANum == typeBNum && typeANum == typeCNum && typeANum == typeDNum && typeANum == typeENum && typeANum == typeFNum && typeANum == typeGNum) {
+            $(".左上（結果）,.右上（結果）,.左側（結果）,.右側（結果）,.左下（結果）,.右下（結果）,.輻輳開散（結果）,.results").fadeIn();
+        } else if( typeANum < typeBNum && typeBNum == typeCNum && typeBNum == typeDNum && typeBNum == typeENum && typeBNum == typeFNum && typeBNum == typeGNum) {
+            $(".右上（結果）,.左側（結果）,.右側（結果）,.左下（結果）,.右下（結果）,.輻輳開散（結果）,.results").fadeIn();
       }
     }
   });
