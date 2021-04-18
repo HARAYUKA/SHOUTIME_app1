875962031cf5c5c5c61a0c0a6aec36d0f4dc582b
kwansai724 <kwansai.taka724@gmail.com>
2021-04-19 07:47:42 +0900

    診断結果の表示仕様修正（複数結果の表示ができるように変更）

--- app/views/diagnoses/index.js
+++ app/views/diagnoses/index.js
@@ -22,26 +22,25 @@ $(function(){
             typeGNum = $(".輻輳開散:checked").length;
         var a = [typeANum, typeBNum, typeCNum, typeDNum, typeENum, typeFNum, typeGNum];
         var max = Math.max.apply(null, a);
-        console.log(max);
-        if( typeANum > typeBNum && typeANum > typeCNum && typeANum > typeDNum && typeANum > typeENum && typeANum > typeFNum && typeANum > typeGNum) {
-            $(".左上（結果）,.results").fadeIn();
-        } else if( typeBNum > typeANum && typeBNum > typeCNum && typeBNum > typeDNum && typeBNum > typeENum && typeBNum > typeFNum && typeBNum > typeGNum) {
-            $(".右上（結果）,.results").fadeIn();
-        } else if( typeCNum > typeANum && typeCNum > typeBNum && typeCNum > typeDNum && typeCNum > typeENum && typeCNum > typeFNum && typeCNum > typeGNum) {
-            $(".左側（結果）,.results").fadeIn();
-        } else if( typeDNum > typeANum && typeDNum > typeBNum && typeDNum > typeCNum && typeDNum > typeENum && typeDNum > typeFNum && typeDNum > typeGNum) {
-            $(".右側（結果）,.results").fadeIn();
-        } else if( typeENum > typeANum && typeENum > typeBNum && typeENum > typeCNum && typeENum > typeDNum && typeENum > typeFNum && typeENum > typeGNum) {
-            $(".左下（結果）,.results").fadeIn();
-        } else if( typeFNum > typeANum && typeFNum > typeBNum && typeFNum > typeCNum && typeFNum > typeDNum && typeFNum > typeENum && typeFNum > typeGNum) {
-            $(".右下（結果）,.results").fadeIn();
-        } else if( typeGNum > typeANum && typeGNum > typeBNum && typeGNum > typeCNum && typeGNum > typeDNum && typeGNum > typeENum && typeGNum > typeFNum) {
-            $(".輻輳開散（結果）,.results").fadeIn();
-        } else if( typeANum == typeBNum && typeANum == typeCNum && typeANum == typeDNum && typeANum == typeENum && typeANum == typeFNum && typeANum == typeGNum) {
-            $(".左上（結果）,.右上（結果）,.左側（結果）,.右側（結果）,.左下（結果）,.右下（結果）,.輻輳開散（結果）,.results").fadeIn();
-        } else if( typeANum < typeBNum && typeBNum == typeCNum && typeBNum == typeDNum && typeBNum == typeENum && typeBNum == typeFNum && typeBNum == typeGNum) {
-            $(".右上（結果）,.左側（結果）,.右側（結果）,.左下（結果）,.右下（結果）,.輻輳開散（結果）,.results").fadeIn();
-      }
+        var eyes = [
+            { type: "A", Num: typeANum, result: ".左上（結果）" },
+            { type: "B", Num: typeBNum, result: ".右上（結果）" },
+            { type: "C", Num: typeCNum, result: ".左側（結果）" },
+            { type: "D", Num: typeDNum, result: ".右側（結果）" },
+            { type: "E", Num: typeENum, result: ".左下（結果）" },
+            { type: "F", Num: typeFNum, result: ".右下（結果）" },
+            { type: "G", Num: typeGNum, result: ".輻輳開散（結果）" },
+          ];
+        
+        var filtered = $.grep(eyes,
+          function(element, index) {
+            
+            return (element.Num >= max);
+          }
+        );
+      
+        let selector = filtered.map(eyes => eyes.result).join(',');
+        $(`${selector},.results`).fadeIn();
     }
   });
 });
